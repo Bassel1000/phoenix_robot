d311f2396707b840539da95569a4865b3619c30c
Bassel1000 <basselashraf@hotmail.com>
2026-06-16 19:55:31 +0300

    Adding dummy_odom

--- ambers_ws/src/phoenix_control/setup.py
+++ ambers_ws/src/phoenix_control/setup.py
@@ -32,6 +32,7 @@ setup(
             'lidar_publisher = phoenix_control.lidar_publisher:main',
             'pump_controller = phoenix_control.pump_controller:main',
             'nozzle_controller = phoenix_control.nozzle_controller:main',
+            'dummy_odom = phoenix_control.dummy_odom:main',
         ],
     },
 )
